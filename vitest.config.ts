3162cf9230a94745035bdff58887bdafc0167012
Pedro Venâncio dos Santos <pedrosantos121229@gmail.com>
2026-03-21 12:43:26 -0300

    feat: configuração de deploy

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', 
+  base: '/portfolio', 
   test: {
     environment: "jsdom",
     globals: true,
